803838cdf35200e6ac26c16950c01c8a20f2fbfe
Aditya Thakur <adityathakurxd@gmail.com>
2023-12-05 12:25:49 +0530

    Improve style of the video call

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,6 @@
 'use client'
-
-import dynamic from 'next/dynamic'
-import '@tldraw/tldraw/tldraw.css'
-import { MakeRealButton } from './components/MakeRealButton'
-import { TldrawLogo } from './components/TldrawLogo'
-import { ResponseShapeUtil } from './ResponseShape/ResponseShape'
-import {
-	HMSRoomProvider,
-	selectIsConnectedToRoom,
-	useHMSActions,
-	useHMSStore,
-} from '@100mslive/react-sdk'
-import JoinForm from './components/JoinForm'
-import { useEffect } from 'react'
-import Conference from './conference/page'
-import Footer from './components/Footer'
+import { HMSRoomProvider } from '@100mslive/react-sdk'
 import Home from './components/Home'
-
-// const Tldraw = dynamic(async () => (await import('@tldraw/tldraw')).Tldraw, {
-// 	ssr: false,
-// })
-
-// const shapeUtils = [ResponseShapeUtil]
 
 export default function App() {
 	return (
@@ -29,13 +8,4 @@ export default function App() {
 			<Home></Home>
 		</HMSRoomProvider>
 	)
-}
-
-{
-	/* <div className="editor">
-				<Tldraw persistenceKey="make-real" shareZone={<MakeRealButton />} shapeUtils={shapeUtils}>
-					<TldrawLogo />
-					
-				</Tldraw>
-			</div> */
 }
