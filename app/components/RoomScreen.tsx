6cf1fd21bf083fb7a0da3dc36e9a47cd0f75a33d
Aditya Thakur <adityathakurxd@gmail.com>
2024-03-13 18:53:23 +0530

    Adds a favicon and removes view poll modal

--- app/components/RoomScreen.tsx
+++ app/components/RoomScreen.tsx
@@ -7,9 +7,7 @@ import {
 	HMSNotificationTypes,
 	HMSPoll,
 } from '@100mslive/react-sdk'
-import Footer from './Footer'
 import Conference from './Conference'
-import ViewPoll from './ViewPoll'
 import PollForm from './PollForm'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -21,17 +19,12 @@ export const RoomScreen = () => {
 	const [pollNotificationData, setPollNotificationData] = useState<HMSPoll | undefined>()
 
 	const [showPollForm, setShowPollForm] = useState(false)
-	const [showPollModal, setShowPollModal] = useState(false)
 	const [questionData, setQuestionData] = useState<any>(undefined)
 
 	const ToastNotification = () => {
 		return (
 			<div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
 				<p>A new poll is available!</p>
-				<button className="primary" onClick={() => setShowPollModal(true)}>
-					{' '}
-					View Poll
-				</button>
 			</div>
 		)
 	}
@@ -57,13 +50,6 @@ export const RoomScreen = () => {
 					onClose={() => {
 						setShowPollForm(false)
 					}}
-				/>
-			)}
-
-			{showPollModal && (
-				<ViewPoll
-					pollNotificationData={pollNotificationData}
-					onClose={() => setShowPollModal(false)}
 				/>
 			)}
 		</QuestionContext.Provider>
